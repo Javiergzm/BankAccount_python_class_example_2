343b36b913c792dd92b01131c24e8d40dfdcbb21
Javiergzm <Javier.guzman@stu.vertexacademies.org>
2026-02-02 18:53:38 +0000

    Fixed all sections and erros, debug complete !!!

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ os.system("clear")
 
 from module import BankAccount
 
-# TODO 1: Account holder names are not being printed correctly in Section 1.
-# TODO 2: Balances are not updating correctly after deposits in Section 3.
-# TODO 3: Withdrawals allow the balance to go negative in Section 2.
-# TODO 4: The greeting method is not displaying anything in Section 1.
+# TODO 1: Account holder names are not being printed correctly in Section 1. DONE
+# TODO 2: Balances are not updating correctly after deposits in Section 3. DONE
+# TODO 3: Withdrawals allow the balance to go negative in Section 2. 
+# TODO 4: The greeting method is not displaying anything in Section 1. DONE
 
 # Create account instances
 a1 = BankAccount("Jordan", 250)
@@ -17,13 +17,13 @@ a2 = BankAccount("Casey", 100)
 print("SECTION 1\n")
 print("Account Holder:", a1.owner)
 print("Balance:", a1.balance)
-a1.greet
+a1.greet()
 
 print()
 
 print("Account Holder:", a2.owner)
 print("Balance:", a2.balance)
-a2.greet
+a2.greet()
 
 print("-----------------------------------\n")
 
--- module.py
+++ module.py
@@ -2,14 +2,18 @@
 
 class BankAccount:
     def __init__(self, owner, balance):
-        self.owner = "owner"
+        self.owner = owner
         self.balance = balance
 
     def greet(self):
         print(f"Welcome, {self.owner}! Your balance is ${self.balance}.")
 
     def deposit(self, amount):
-        self.balance + amount
+        self.balance = self.balance + amount
 
     def withdraw(self, amount):
-        self.balance = self.balance - amount
+            if self.balance < amount:
+                print("Insufficient funds. Withdrawal canceled.")
+            if self.balance >= amount:
+                self.balance = self.balance - amount
+            
